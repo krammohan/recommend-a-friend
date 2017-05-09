3e54a900853e50f6252dfc482cfd357845ecd40c
krammohan <kavya.ramm@gmail.com>
2017-05-09 14:07:53 -0700

    Adds routes for login signup and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "categories#index"
+  resources :categories do
+    resources :articles
+  end
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 end
