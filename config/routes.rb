bc5c6c86b2dfe9cd6b050968085737fbeaec5dbe
krammohan <kavya.ramm@gmail.com>
2017-05-09 14:13:40 -0700

    Adds table recommendations

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "categories#index"
-  resources :categories do
-    resources :articles
-  end
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
--- db/migrate/20170509205550_create_recommendations.rb
+++ db/migrate/20170509205550_create_recommendations.rb
@@ -1,7 +1,10 @@
 class CreateRecommendations < ActiveRecord::Migration[5.0]
   def change
     create_table :recommendations do |t|
-
+      t.integer :user_id, :null => false
+  	  t.integer :recommended_id, :null => false
+  	  t.integer :recommender_id, :null => false
+  	  
       t.timestamps
     end
   end
